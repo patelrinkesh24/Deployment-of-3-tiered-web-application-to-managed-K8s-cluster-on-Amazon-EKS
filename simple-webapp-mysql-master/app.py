e2041b8f96ffafef7a78c4f530eca616e384094a
Muskan Katariya <64361475+tech-girl123@users.noreply.github.com>
2022-08-15 21:56:07 -0400

    downloaded images locally using python

--- simple-webapp-mysql-master/app.py
+++ simple-webapp-mysql-master/app.py
@@ -3,17 +3,19 @@ from flask import render_template
 import socket
 import mysql.connector
 import os
-
+import boto3
 app = Flask(__name__)
 
-DB_Host = os.environ.get('DB_Host') or "localhost"
+DB_Host = os.environ.get('MYSQL_SERVICE_HOST') or "localhost"
 DB_Database = os.environ.get('DB_Database') or "mysql"
 DB_User = os.environ.get('DB_User') or "root"
 DB_Password = os.environ.get('DB_Password') or "paswrd"
 USER_NAME = os.environ.get('USER_NAME') or "Specify User Name"
-IMAGE = os.environ.get('IMAGE_URL') or "IMAGEURL"
+IMAGE_URL = os.environ.get('IMAGE_URL') or "IMAGEURL"
 
-
+s3 = boto3.client('s3')
+s3.download_file('images-ass4', 'success.jpg', 'static/success.jpg')
+s3.download_file('images-ass4', 'failed.png', 'static/failed.png')
 @app.route("/")
 def main():
     db_connect_result = False
@@ -25,13 +27,12 @@ def main():
     except Exception as e:
         color = '#ff3f3f'
         err_message = str(e)
-    return render_template('hello.html', name=a, path=path)
-    #return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, USER_NAME=USER_NAME, IMAGE_URL=IMAGE_URL)
 
 @app.route("/debug")
 def debug():
     color = '#2196f3'
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color, USER_NAME=USER_NAME, IMAGE_URL=IMAGE_URL)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
